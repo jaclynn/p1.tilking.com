d997838238331cec4a503d20901b4ad0df6ec193
Jacqueline Wilson <jwilson@fas.harvard.edu>
2013-09-12 19:21:11 -0400

    Forgot semi colon

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
 <?php echo '<h1>Hello World!
 by Jacqueline Wilson, CSCIE-15
-Distance Student, Landenberg, PA</h1>' ?>
+Distance Student, Landenberg, PA</h1>
+<br/>Added semi colon to php statement :P'; ?>
