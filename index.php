c6aea1e25cd69ceb6c5786f026fc126c9dd161af
Jacqueline Wilson <jwilson@fas.harvard.edu>
2013-09-12 19:56:35 -0400

    Readded PHP after permissions problem

--- index.php
+++ index.php
@@ -1,11 +1,14 @@
 <html>
 <head></head>
 <body>
-<h1>Hello World!
+<?php echo 
+'<h1>Hello World!
 by Jacqueline Wilson, CSCIE-15
 Distance Student, Landenberg, PA</h1>
 <br/>Added semi colon to php statement :P
 <br/>PHP *still* not working but works locally. Adding external html tags, sigh.
 <br/>Enough. Removing PHP.
+<br/>Adding PHP back in; apparently Git changed the permissions on index.php to 664; 
+needs to be 644.'; ?>
 </body>
 </html>
