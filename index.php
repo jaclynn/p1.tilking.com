ce083d3a39461d4eed4a0b9860d7b7c19c5afee5
Jacqueline Wilson <jwilson@fas.harvard.edu>
2013-09-12 19:32:53 -0400

    Removed PHP. This is ridiculous.

--- index.php
+++ index.php
@@ -1,10 +1,11 @@
 <html>
 <head></head>
 <body>
-<?php echo '<h1>Hello World!
+<h1>Hello World!
 by Jacqueline Wilson, CSCIE-15
 Distance Student, Landenberg, PA</h1>
 <br/>Added semi colon to php statement :P
-<br/>PHP *still* not working but works locally. Adding external html tags, sigh.'; ?>
+<br/>PHP *still* not working but works locally. Adding external html tags, sigh.
+<br/>Enough. Removing PHP.
 </body>
 </html>
