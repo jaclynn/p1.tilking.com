ac5b67d0bf4712cbd84bf0424a0c653d0c0696cb
Jacqueline Wilson <jwilson@fas.harvard.edu>
2013-09-12 19:30:44 -0400

    PHP does not like the orange. Annoying.

--- index.php
+++ index.php
@@ -1,4 +1,10 @@
+<html>
+<head></head>
+<body>
 <?php echo '<h1>Hello World!
 by Jacqueline Wilson, CSCIE-15
 Distance Student, Landenberg, PA</h1>
-<br/>Added semi colon to php statement :P'; ?>
+<br/>Added semi colon to php statement :P
+<br/>PHP *still* not working but works locally. Adding external html tags, sigh.'; ?>
+</body>
+</html>
