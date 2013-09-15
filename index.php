84834a5d832e662b2fa3be21173166623f10b1bc
Jacqueline Wilson <jwilson@fas.harvard.edu>
2013-09-15 11:39:39 -0400

    No CSS yet

--- index.php
+++ index.php
@@ -1,16 +1,26 @@
+<!doctype html>
 <html>
-<head></head>
+<head>
+<meta charset="UTF-8">
+<title>Jacqueline Wilson P1</title>
+<style type="text/css">
+
+</style>
+</head>
+
 <body>
-<?php echo 
-'<h1>Hello World!
-by Jacqueline Wilson, CSCIE-15
-Distance Student, Landenberg, PA</h1>
-<br/>Added semi colon to php statement :P
-<br/>PHP *still* not working but works locally. Adding external html tags, sigh.
-<br/>Enough. Removing PHP.
-<br/>Adding PHP back in; apparently Git changed the permissions on index.php to 664; 
-needs to be 644.
-<br/>Testing to see if the post-merge trick worked.
-<br/>Test of Federico fix'; ?>
+<p><img src="jackie.jpg" width="285" height="307"  alt=""/><h1 id="myname">Jacqueline Wilson</h1>
+<h2>About Me</h2>
+<p>Hi everyone. My name is Jacqueline Wilson and I live in a small, old farmhouse in Landenberg, PA with my husband, son, two dogs and five cats. I work at my son's school as a technology director. I started HES in Spring 2011 with Len Evenchik's Communication and Internet Protocols course and loved it. I've since changed status to degree candidate, and I have five classes to go. I graduated high school (a long time ago) in Taipei, Taiwan. I have a computer science degree from University of Delaware. I use a Macbook Pro because it's the only machine that gives me access to all three operating systems I need to support. Plus, I hate Windows 8. </p>
+<h2>Professional</h2>
+<p><a href="http://www.linkedin.com/pub/jacqueline-wilson/5/58b/471">Jacqueline Wilson's LinkedIn Profile</a></p>
+<h2>Interests</h2>
+<p>Sewing/Quilting<br>
+Piano<br>
+Shooting pool</p>
+<h2>Code Editors</h2>
+<p>Sublime Text and Text Wrangler for simple edits<br>
+  Dreamweaver because I have no artistic/layout/design ability whatsoever.
+</p>
 </body>
 </html>
