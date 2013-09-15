8bd911d3659707ae87aeac09ef2c36b26368d467
Jacqueline Wilson <jwilson@fas.harvard.edu>
2013-09-15 12:33:33 -0400

    With CSS

--- index.php
+++ index.php
@@ -3,24 +3,77 @@
 <head>
 <meta charset="UTF-8">
 <title>Jacqueline Wilson P1</title>
+<!--  Jacqueline Wilson
+      CSCI E-15, Project 1
+-->
+<?php /* Just adding PHP code block to test */?>
 <style type="text/css">
-
+p {
+	font-family: "Gill Sans", "Gill Sans MT", "Myriad Pro", "DejaVu Sans Condensed", Helvetica, Arial, sans-serif;
+}
+h1 {
+	font-family: Gotham, "Helvetica Neue", Helvetica, Arial, sans-serif;
+}
+h2 {
+	font-family: Gotham, "Helvetica Neue", Helvetica, Arial, sans-serif;
+}
+body {
+	background-color: #063201;
+	color: #CCCCCC;
+}
+a {
+	color: #996600;
+}
+img {
+	float: left;
+	padding-top: 15px;
+	padding-right: 15px;
+	padding-bottom: 15px;
+	padding-left: 15px;
+}
+table, tr, td, th {
+	border: 0px  #063201;
+	padding-top: 0px;
+	padding-bottom: 15px;
+}
 </style>
 </head>
 
 <body>
-<p><img src="jackie.jpg" width="285" height="307"  alt=""/><h1 id="myname">Jacqueline Wilson</h1>
-<h2>About Me</h2>
-<p>Hi everyone. My name is Jacqueline Wilson and I live in a small, old farmhouse in Landenberg, PA with my husband, son, two dogs and five cats. I work at my son's school as a technology director. I started HES in Spring 2011 with Len Evenchik's Communication and Internet Protocols course and loved it. I've since changed status to degree candidate, and I have five classes to go. I graduated high school (a long time ago) in Taipei, Taiwan. I have a computer science degree from University of Delaware. I use a Macbook Pro because it's the only machine that gives me access to all three operating systems I need to support. Plus, I hate Windows 8. </p>
-<h2>Professional</h2>
-<p><a href="http://www.linkedin.com/pub/jacqueline-wilson/5/58b/471">Jacqueline Wilson's LinkedIn Profile</a></p>
-<h2>Interests</h2>
-<p>Sewing/Quilting<br>
-Piano<br>
-Shooting pool</p>
-<h2>Code Editors</h2>
-<p>Sublime Text and Text Wrangler for simple edits<br>
-  Dreamweaver because I have no artistic/layout/design ability whatsoever.
-</p>
+<p>
+  <img src="jackie.jpg" width="285" height="307"  alt=""/>
+  <h1>Jacqueline Wilson</h1>
+  <table width="931" border="1">
+  <tr>
+    <td width="921"><h2>About Me</h2>
+    <p>Hi everyone. My name is Jacqueline Wilson and I live in a small, 
+      old farmhouse in Landenberg, PA with my husband, son, two dogs and 
+      five cats. I work at my son's school as a technology director. I 
+      started HES in Spring 2011 with Len Evenchik's Communication and 
+      Internet Protocols course and loved it. I've since changed status 
+      to degree candidate, and I have five classes to go. I graduated high 
+      school (a long time ago) in Taipei, Taiwan. I have a computer science 
+      degree from University of Delaware. I use a Macbook Pro because it's 
+      the only machine that gives me access to all three operating systems 
+      I need to support. Plus, I hate Windows 8. </p></td>
+  </tr>
+  <tr>
+    <td>
+      <h2>Professional</h2>
+      <p><a href="http://www.linkedin.com/pub/jacqueline-wilson/5/58b/471">Jacqueline Wilson's LinkedIn Profile</a></p>
+    </td>
+  </tr>
+  <tr>
+    <td><h2>Interests</h2>
+      <p>Sewing/Quilting<br>Piano<br>Shooting pool</p>
+    </td>
+  </tr>
+  <tr>
+    <td><h2>Code Editors</h2>
+      <p>Sublime Text and Text Wrangler for simple edits<br>
+    Dreamweaver because I have no artistic/layout/design ability whatsoever.</p>
+  </td>
+  </tr>
+</table>
 </body>
 </html>
